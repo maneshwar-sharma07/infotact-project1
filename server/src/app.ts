c45494bcf3177d64b05ff7efcc77f88559ef2b9d
maneshwar-sharma07 <maneshwar.sharma07@gmail.com>
2026-06-15 13:30:08 +0530

    feat:implement authentication routes and controllers

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,4 +1,6 @@
-import express from "express";
+import express from 'express';
+import authRoutes from './routes/auth.routes';
 const app =express();
 app.use(express.json());
+app.use('/api/auth', authRoutes);
 export default app;
