f9b518521e40ed41c1eaffbace1f1e14d55dc895
maneshwar-sharma07 <maneshwar.sharma07@gmail.com>
2026-06-17 12:31:03 +0530

    feat:integrate channel APIs

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,12 @@
 import express from 'express';
 import authRoutes from './routes/auth';
 import workspaceRoutes from './routes/workspace.routes';
+import channelRoutes from './routes/channel.routes';
+
+
 const app =express();
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/workspaces', workspaceRoutes);
+app.use('/api/channels', channelRoutes);
 export default app;
