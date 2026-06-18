90af2b83f8711421760a408d743390552344a6e8
maneshwar-sharma07 <maneshwar.sharma07@gmail.com>
2026-06-18 20:52:42 +0530

    feat:implement message APIs

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,11 +2,13 @@ import express from 'express';
 import authRoutes from './routes/auth';
 import workspaceRoutes from './routes/workspace.routes';
 import channelRoutes from './routes/channel.routes';
-
+import messageRoutes from './routes/message.routes';
 
 const app =express();
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/workspaces', workspaceRoutes);
 app.use('/api/channels', channelRoutes);
+app.use('/api/messages', messageRoutes);
+
 export default app;
