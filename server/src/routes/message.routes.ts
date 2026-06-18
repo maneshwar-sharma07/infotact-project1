ec6db428280c3751c35957da2de9616a6d7ed025
maneshwar-sharma07 <maneshwar.sharma07@gmail.com>
2026-06-18 20:50:05 +0530

    feat:implement message routes

---
+++ server/src/routes/message.routes.ts
@@ -0,0 +1,16 @@
+import {Router} from 'express';
+import {
+    createMessage,
+    getMessages,
+} from '../controllers/message.controller';
+
+import {verifyToken} from '../middlewares/verifyToken';
+
+const router = Router();
+
+router.use(verifyToken);
+
+router.get("/",getMessages);
+router.post("/",createMessage);
+
+export default router;
